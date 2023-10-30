6481c9b5b516dcdb4f6a02a9779e9f466333ffcc
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-10-30 19:09:48 +0300

    responsive card created

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import MainLayout from "../../components/MainLayout";
 
 import Hero from "./container/Hero";
+import Articles from "./container/Articles";
 
 const HomePage = () => {
   return (
     <div>
       <MainLayout>
         <Hero />
+        <Articles />
       </MainLayout>
     </div>
   );
