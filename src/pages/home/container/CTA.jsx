8f2c0ab580327f89853032a43d71e60513a4d0c2
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-11-01 02:14:14 +0300

    CTA

--- src/pages/home/container/CTA.jsx
+++ src/pages/home/container/CTA.jsx
@@ -22,23 +22,23 @@ const CTA = () => {
       </svg>
 
       <section className="relative bg-dark-hard px-5">
-        <div className="container grid grid-cols-12 mx-auto py-10">
-          <div className="col-span-12">
-            <h2 className="text-white font-roboto font-bold text-2xl">
+        <div className="container grid grid-cols-12 mx-auto py-10 md:pb-20 lg:place-items-center">
+          <div className="col-span-12 lg:col-span-6">
+            <h2 className="text-white font-roboto font-bold text-2xl md:text-4xl md:text-center md:leading-normal lg:text-left">
               Get our stories delivered From us to your inbox weekly.
             </h2>
-            <div className="w-full max-w-[494px] mt-12 space-y-3 mx-auto">
+            <div className="w-full max-w-[494px] mt-12 space-y-3 mx-auto md:space-y-0 md:flex md:items-center md:space-x-2 lg:mx-0">
               <input
                 type="text"
-                className="px-4 py-3 rounded-lg w-full placeholder:text-dark-light focus:outline-none"
+                className="px-4 py-3 rounded-lg w-full placeholder:text-dark-light"
                 placeholder="Your Email"
               />
-              <button className="px-4 py-3 rounded-lg w-full bg-primary text-white font-bold">
+              <button className="px-4 py-3 rounded-lg w-full bg-primary text-white font-bold md:w-fit md:whitespace-nowrap">
                 Get started
               </button>
             </div>
-            <p className=" text-dark-light text-sm leading-7 mt-6 lg:mt-10 lg:text-center">
-              <span className="text-[#B3BAC5] font-bold italic">
+            <p className="text-dark-light text-sm leading-7 mt-6 md:text-center md:text-base lg:text-left">
+              <span className="font-bold italic text-[#B3BAC5] md:not-italic md:font-normal md:text-dark-light">
                 Get a response tomorrow
               </span>{" "}
               if you submit by 9pm today. If we received after 9pm will get a
@@ -47,12 +47,23 @@ const CTA = () => {
           </div>
           <div className="col-span-12 hidden mb-[70px] md:block md:order-first lg:col-span-6 lg:order-last">
             <div className="w-3/4 mx-auto relative">
-              <div className="w-full rounded-lg bg-white p-3 z-[1] relative">
+              <div className="w-1/2 h-1/2 bg-[#FC5A5A] rounded-lg absolute top-[10%] -right-[8%]" />
+              <div className="w-1/2 h-1/2 bg-white rounded-lg opacity-[.06] absolute -bottom-[10%] -left-[8%]" />
+              <div className="w-full rounded-xl bg-white p-3 z-[1] relative">
                 <img
                   src={images.CTAImage}
-                  alt="product"
+                  alt="title"
                   className="w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60"
                 />
+                <div className="p-5">
+                  <h2 className="font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]">
+                    Future of Work
+                  </h2>
+                  <p className="text-dark-light mt-3 text-sm md:text-lg">
+                    Majority of people will work in jobs that don&apos;t exist
+                    today.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
