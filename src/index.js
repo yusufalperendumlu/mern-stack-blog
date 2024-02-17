1567a0dcda9400eb5b04ced9b7c4bec8cdd4df3f
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-02-17 19:57:11 +0300

    Add QueryClientProvider for React Query

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </BrowserRouter>
 );
