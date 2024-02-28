f8fd35733f2ecc262d74dce62eb36c86b6584766
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-02-29 02:05:02 +0300

    Add react-easy-crop component and related files

---
+++ src/components/crop/CropEasy.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CropEasy = () => {
+  return (
+    <div className="fixed z-[1000] inset-0 bg-black/50 flex justify-center p-5 overflow-auto"></div>
+  );
+};
+
+export default CropEasy;
