2619c347344d82ec549cf4319c6402e0af27e760
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-03-01 19:02:00 +0300

    Update button styles in Articles component

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -10,7 +10,7 @@ const Articles = () => {
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
       </div>
-      <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
+      <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg hover:bg-primary hover:text-white transition-all duration-300 ease-linear">
         <span>More article</span>
         <FaArrowRight className="w-3 h-3" />
       </button>
