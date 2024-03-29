b67052e508efc475e5803c3a65ac1d8074799f14
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-03-29 03:31:26 +0300

    Add SampleImage and UserAvatar to images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,6 +4,8 @@ import Post1Image from "../assets/posts/post1.jpg";
 import ProfileImage from "../assets/posts/post-profile.svg";
 import CTAImage from "../assets/CtaImage.jpg";
 import DeleteImage from "../assets/delete/Garbage.svg";
+import SampleImage from "../assets/Sample.webp";
+import UserAvatar from "../assets/userAvatars.png";
 
 const images = {
   Logo,
@@ -12,6 +14,8 @@ const images = {
   ProfileImage,
   CTAImage,
   DeleteImage,
+  SampleImage,
+  UserAvatar,
 };
 
 export default images;
